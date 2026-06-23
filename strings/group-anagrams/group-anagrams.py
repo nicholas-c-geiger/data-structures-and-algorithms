63161d4a8473e76eb9cb3de773f939b4262f10bd
Nicholas Geiger <nicholas.c.geiger@outlook.com>
2026-06-23 11:28:29 -0700

    Implement groupAnagrams function in group-anagrams.py

---
+++ strings/group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map: defaultdict[str, List[str]] = defaultdict(list)
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            map[key].append(s)
+
+        return list(map.values())
